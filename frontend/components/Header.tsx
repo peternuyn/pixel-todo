b5dfcf00be14c7997fb567588f96c0f377e40f2d
Peter Nguyen <peternguyen@MacBookPro.lan>
2026-05-31 22:14:04 +0700

    fix: change to the address of the logo in the code

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,10 +1,10 @@
-import PixelCatBadge from "./pixel/PixelCatBadge";
-
+import Image from "next/image";
+import Logo from "@/assets/logo.svg";
 export default function Header() {
   return (
-    <header className="flex items-end gap-4 mb-5 max-w-[1240px] mx-auto pl-16 w-full">
+    <header className="flex items-end gap-4 mb-5 max-w-[1240px] mx-auto pl-16 w-full bg-transparent">
       <div className="flex items-center gap-3">
-        <PixelCatBadge />
+        <Image className="w-20 h-20" src={Logo} alt="Meowdow logo" />
         <div>
           <h1 className="font-jersey text-5xl leading-none text-ink [text-shadow:3px_3px_0_#FFE89A]">
             Meowdow Study Farm
